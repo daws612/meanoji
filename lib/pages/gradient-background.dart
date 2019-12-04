9cce887f7583287ed1b768f9dbca9168725b68b0
Firdaws <firdaws.farukh@hotmail.com>
2019-12-04 22:13:00 +0300

    create initial splash screen

---
+++ lib/pages/gradient-background.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations/controlled_animation.dart';
+import 'package:simple_animations/simple_animations/multi_track_tween.dart';
+
+class GradientBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("color1").add(Duration(seconds: 3),
+          ColorTween(begin: Colors.purple[300], end: Colors.purple[800])),
+      Track("color2").add(Duration(seconds: 3),
+          ColorTween(begin: Color(0xffA83279), end: Colors.blue.shade600))
+    ]);
+
+    return ControlledAnimation(
+      playback: Playback.MIRROR,
+      tween: tween,
+      duration: tween.duration,
+      builder: (context, animation) {
+        return Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.purple[300],
+              Colors.purple[500],
+              Colors.purple[700],
+              Colors.purple[800]])),
+        );
+      },
+    );
+  }
+}
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:meanoji/pages/animated-wave.dart';
+import 'package:meanoji/pages/gradient-background.dart';
 
-class Splash extends StatefulWidget{
+class Splash extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new SplashState();
-  }
-    
 }
 
-class SplashState extends State<Splash>{
+class SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
+    double pi = 3.142;
+
     return new Scaffold(
-      appBar: AppBar(
-        title: Text("Meanoji"),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            stops: [0.1, 0.5, 0.7, 0.9],
-            colors: [
-            // Colors are easy thanks to Flutter's Colors class.
-            Colors.purple[800],
-            Colors.purple[700],
-            Colors.purple[600],
-            Colors.purple[400],
-          ],
-          )
+        appBar: AppBar(
+          title: Text("Meanoji"),
         ),
-      ),
-    );
+        body: Stack(
+          children: <Widget>[
+            Positioned.fill(child: GradientBackground()),
+            onBottom(AnimatedWave(
+              height: 180,
+              speed: 1.0,
+            )),
+            onBottom(AnimatedWave(
+              height: 120,
+              speed: 0.9,
+              offset: pi,
+            )),
+            onBottom(AnimatedWave(
+              height: 220,
+              speed: 1.2,
+              offset: pi / 2,
+            )),
+            Positioned.fill(child: Text("")),
+          ],
+        ));
   }
-  
+
+  onBottom(Widget child) => Positioned.fill(
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: child,
+        ),
+      );
 }
