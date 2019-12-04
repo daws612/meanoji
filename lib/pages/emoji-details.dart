d6aee7d18fd14c6f9f2fd7ac260414e4263ccbba
Firdaws <firdaws.farukh@hotmail.com>
2019-12-05 00:18:13 +0300

    Add emojis. Initial implementation of details page

---
+++ lib/pages/emoji-details.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class EmojiDetails extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new EmojiDetailsState();
+}
+
+class EmojiDetailsState extends State<EmojiDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 400.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text("Collapsing Toolbar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      )),
+                  background: Image(
+                    image: new AssetImage('openmoji618/1F60A.png'),
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Sample Text"),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:meanoji/pages/animated-wave.dart';
+import 'package:meanoji/pages/emoji-details.dart';
 import 'package:meanoji/pages/gradient-background.dart';
 
 class Splash extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new SplashState();
+
+  void onLoad(BuildContext context) async {
+    openEmojiDetailsFuture().then((value) {
+      //ew EmojiDetails();
+      Navigator.of(context).push(_createRoute());
+    }, onError: (error) {
+      print(error);
+    }).timeout(Duration(seconds: 6), onTimeout: () {
+      print('6 secs timed out'); //TODO run tests for this
+    });
+  }
+
+  Future<bool> openEmojiDetailsFuture() async {
+    await Future.delayed(Duration(seconds: 5));
+    return true;
+  }
+
+  Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => EmojiDetails(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
 }
 
 class SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    widget.onLoad(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     double pi = 3.142;
@@ -33,7 +74,24 @@ class SplashState extends State<Splash> {
               speed: 1.2,
               offset: pi / 2,
             )),
-            Positioned.fill(child: Text("")),
+            Positioned.fill(
+                child: Container(
+                    height: 618,
+                    width: 618,
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: <Widget>[
+                        new Image(
+                          //parte importante, definire gli asset per trovarli più velocemnte
+                          //si inseriscono nel pubspec.yaml
+                          image: new AssetImage('openmoji618/1F60A.png'),
+                        ),
+                        Text(
+                          'Welcome!',
+                          style: TextStyle(fontSize: 40, color: Colors.yellow),
+                        ),
+                      ],
+                    ))),
           ],
         ));
   }
